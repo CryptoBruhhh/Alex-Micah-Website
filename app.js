b7164e6d66105c9a65476724efa4bcc4c747a0c9
Xettman (Desktop) <xettman777@gmail.com>
2024-08-08 19:50:34 +1000

    Improved MongoDB URI in app.js

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const User = require('./models/User');
 
 const app = express();
 
-// Connect to DABASE HERE. (chose mongodb, i prefer sql oracle tho)
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -20,7 +19,6 @@ mongoose.connect(process.env.MONGO_URI, {
   .catch(err => {
       console.error('Failed to connect to MongoDB', err);
   });
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
