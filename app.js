6b4f066cfe0e84f2da476b2ebef29752c63671c7
Xettman (Desktop) <xettman777@gmail.com>
2024-08-08 19:44:59 +1000

    Fixed mongoDB to run in the docker not locally

--- app.js
+++ app.js
@@ -9,13 +9,18 @@ const User = require('./models/User');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/simple-auth')
+// Connect to DABASE HERE. (chose mongodb, i prefer sql oracle tho)
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
   .then(() => {
       console.log('Connected to MongoDB');
   })
   .catch(err => {
       console.error('Failed to connect to MongoDB', err);
   });
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
